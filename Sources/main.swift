095a8c114cdff27cf466e4fbf56a7fe6562c71c3
Jacopo Mangiavacchi <Jacopo.Mangiavacchi@uk.ibm.com>
2016-09-28 15:11:15 +0100

    v.0.1.0

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,18 +1,18 @@
 /**
-* Copyright IBM Corporation 2016
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 
 // Kitura-Starter-Bluemix shows examples for creating custom routes.
 import Foundation
@@ -22,6 +22,7 @@ import KituraNet
 import LoggerAPI
 import HeliumLogger
 import CloudFoundryEnv
+import KituraBot
 import KituraBotFacebookMessenger
 //import CloudFoundryDeploymentTracker
 
@@ -39,30 +40,61 @@ Log.logger = HeliumLogger()
 router.all("/", middleware: StaticFileServer())
 
 
-// Change your Facebook Messenger Bot configuration in the Configuration.swift file
-let poc = KituraBotFacebookMessenger(appSecret: Configuration.appSecret, validationToken: Configuration.validationToken, pageAccessToken: Configuration.pageAccessToken, webHookPath: "/webhook", sendApiPath: "/sendmessage", router: router) { (senderId: String, message: String) -> String? in
+//1. Instanciate KituraBot and implement BOT logic
+let bot = KituraBot(router: router) { (channelName: String, senderId: String, message: String) -> String? in
     
-    //Implement your BOT logic and return a response message
+    //1.a Implement classic Syncronous BOT logic implementation with Watson Conversation, api.ai, wit.ai or other tools
     let responseMessage = "ECHO: \(message)"
-
-    print("Received message: \(message)")
-    print("Response message: \(responseMessage)")
     
+    ///3.a [Optional] Manage classic Asyncronous BOT logic implementation decoupling for example with OpenWhisk
+    //let openWhiskMessage = ["channelName" : channelName, "senderId" : senderId, "message" : message]
+    //whisk.fireTrigger(name: "xx", package: "xx", namespace: "xx", parameters: openWhiskMessage, callback: {(reply, error) -> Void in {}
+    /// OpenWhisk chain will use a specific KituraBotPushAction to send back to KituraBot in a asyncronous way the response message to send back to client
+    
+    //1.b return immediate Syncronouse response or return nil to do not send back any Syncronous response message
     return responseMessage
+}
+
+
+/////3.b [Optional] Activate Async Push Back cross channel functionality
+//bot.exposePushBack() {(channelName: String, senderId: String, message: String) -> Void? in
+//    /// The implementation of exposePushBack method in KituraBot class will automatically expose REST interface to be called by the Async logic (i.e. KituraBotPushAction)
+//
+//    var responseChannelName = channelName
+//
+//    ///3.c [Optional] implement optional logic to eventually notify back the user on different channels
+//    responseChannelName = "..."
+//
+//    ///3.d [Optional] send back Async response message
+//    bot.sendPushBack(channelName: responseChannelName, senderId: senderId, message: message)
+//}
+
+
+//2. Add specific channel to the KituraBot instance
+do {
+    //2.1 Add Facebook Messenger channel
+    try bot.addChannel(channelName: "FacebookEcho", channel: KituraBotFacebookMessenger(appSecret: Configuration.appSecret, validationToken: Configuration.validationToken, pageAccessToken: Configuration.pageAccessToken, webHookPath: "/webhook"))
     
-    //return nil //to do not send back any response message
+    //2.1 Add Slack, Skype etc. channels
+    //try bot.addChannel(channelName: "SlackEcho1", KituraBotSlack(slackConfig: "xxx", webHookPath: "/echo1slackcommand"))
+    //try bot.addChannel(channelName: "SkypeEcho1", KituraBotSkype(skypeConfig: "xxx", webHookPath: "/echo1skypewebhook"))
+    
+    //2.2 Add MobileApp channel (use Push Notification for Asyncronous response message
+    //try bot.addChannel(channelName: "MobileAppEcho1", KituraBotMobileApp("appSecret": "yyy-yyy-yyy", "appId": "xxx-xxx-xx", apiPath: "/echo1api")))
+} catch is KituraBotError {
+    Log.error("Oops... something wrong on Bot Channel name")
 }
 
 
 // Start Kitura-Starter-Bluemix server
 do {
-  let appEnv = try CloudFoundryEnv.getAppEnv()
-  let port: Int = appEnv.port
-  Log.info("Server will be started on '\(appEnv.url)'.")
-  //CloudFoundryDeploymentTracker(repositoryURL: "https://github.com/IBM-Swift/Kitura-Starter-Bluemix.git", codeVersion: nil).track()
-  Kitura.addHTTPServer(onPort: port, with: router)
-  Kitura.run()
+    let appEnv = try CloudFoundryEnv.getAppEnv()
+    let port: Int = appEnv.port
+    Log.info("Server will be started on '\(appEnv.url)'.")
+    //CloudFoundryDeploymentTracker(repositoryURL: "https://github.com/IBM-Swift/Kitura-Starter-Bluemix.git", codeVersion: nil).track()
+    Kitura.addHTTPServer(onPort: port, with: router)
+    Kitura.run()
 } catch CloudFoundryEnvError.InvalidValue {
-  Log.error("Oops... something went wrong. Server did not start!")
+    Log.error("Oops... something went wrong. Server did not start!")
 }
 
