a0cf5338092dec28e4946c8a08a079906648adbd
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-09-29 10:38:44 +0100

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -22,8 +22,8 @@ import KituraNet
 import LoggerAPI
 import HeliumLogger
 import CloudFoundryEnv
-//import KituraBot
-//import KituraBotFacebookMessenger
+import KituraBot
+import KituraBotFacebookMessenger
 //import CloudFoundryDeploymentTracker
 
 // Disable all buffering of stdout
