a69e71e4974dd81435ff8295393a8f19f07f2c60
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-10-03 15:57:17 +0100

    added context and mobileapi channel

--- Package.swift
+++ Package.swift
@@ -30,6 +30,7 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/JacopoMangiavacchi/KituraBot.git", majorVersion: 0),
-        .Package(url: "https://github.com/JacopoMangiavacchi/KituraBotFacebookMessenger.git", majorVersion: 0)
+        .Package(url: "https://github.com/JacopoMangiavacchi/KituraBotFacebookMessenger.git", majorVersion: 0),
+        .Package(url: "https://github.com/JacopoMangiavacchi/KituraBotMobileAPI.git", majorVersion: 0)
     ],
     exclude: ["Makefile", "Package-Builder"])
