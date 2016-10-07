b33f6f9dff70cda36b1afac9604dddbc1cc4aefb
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-10-07 17:47:24 +0100

    Update Configuration.swift

--- Sources/Configuration.swift
+++ Sources/Configuration.swift
@@ -19,26 +19,42 @@
  */
 public struct Configuration {
     // App Secret can be retrieved from the App Dashboard
-    static let appSecret = "4c36e757ed0976a2856564976aac973d"
+    static let appSecret = "xxx"
 
     // Arbitrary value used to validate a webhook
-    static let validationToken = "JacopoMangiavacchiToken"
+    static let validationToken = "xxx"
 
     // Generate a page access token for your page from the App Dashboard
-    static let pageAccessToken = "EAAIlxjmRZB7YBADlpLZCugk0wku7qprQK2oOEMSPnUZBQVXyt1VwW2o4BPciBTu6eE9jUOAei7ZAcnXIg7g0EiG88QtQEJWDfU4q1dRZAtVmKQSOyF63LZCk111ldKO7cfd6PP5AZBp4pFssPYz4QrrpfUGkG4es7w0zaDQZAJrRZAAZDZD"
-    
+    static let pageAccessToken = "xxx"
+
+    // Hosted BOT Server URL
+    static let botServerUrl = "http://localhost:8090"
+
+    // Async push back path
+    static let pushApiPath = "/botpushapi"
+
     // Token to use for verifing access to the Async Push Notification API
-    static let pushApiSecurityToken = "1234"
+    static let pushApiSecurityToken = "xxx"
     
+    // Async push back path
+    static let mobileApiPath = "/mobileapi"
+
     // Token to use for verifing access to the Mobile API
-    static let mobileApiSecurityToken = "1234"
+    static let mobileApiSecurityToken = "xxx"
 
-    // Push certificate path
-    static let mobileApiPushPassPath = "./Certificates/apns-dev.crt.pem"
+    // Facebook Messenger Channel Name
+    static let facebookChanelName = "FacebookEcho"
+
+    // Mobile API Channel Name
+    static let mobileAPIChanelName = "MobileAppEcho"
+
+
+    // Push Bluemix Region
+    static let mobileApiPushBluemixRegion = "ng.bluemix.net"
     
-    // Push key path
-    static let mobileApiPushKeyPath = "./Certificates/apns-dev.key.pem"
-
-    // Push topic
-    static let mobileApiPushTopic = "com.jacopomangiavacchi.KituraBot"
+    // Push Bluemix AppGuid
+    static let mobileApiPushBluemixAppGuid = "xxx"
+    
+    // Push Bluemix AppSecret
+    static let mobileApiPushBluemixAppSecret = "xxx"
 }
